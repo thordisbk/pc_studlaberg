c3471740966f61348904581ec1b0c599df75d3f5
thordisbk <pokr@itu.dk>
2020-05-08 12:53:59 +0000

    test points

--- Assets/Scripts/RandomPoints.cs
+++ Assets/Scripts/RandomPoints.cs
@@ -15,13 +15,15 @@ public class RandomPoints
         // returns a Vector2 array of random values on a specified plane
 
         Vector2[] arr = new Vector2[num];
-        for (int i = 0; i < num; i++) {
+        /*for (int i = 0; i < num; i++) {
             float x = Random.Range(0f, maxX);
             float y = Random.Range(0f, maxY);
             Vector2 newPoint = new Vector2(x, y);
             arr[i] = newPoint;
             if (verbose) Debug.Log("newPoint: " + newPoint);
-        }
+        }*/
+        arr[0] = new Vector2(4.7f, 4.7f);
+        if (num > 1) arr[1] = new Vector2(3.2f, 2.2f);
         return arr;
     }
 }
